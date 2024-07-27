4803d9b9597e5ea0842e0fe2bb38455661053948
pixellateddev <pixellateddev@proton.me>
2024-07-28 00:41:40 +0530

    multizone support

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,4 +1,20 @@
+const docsURL = process.env.NEXT_PUBLIC_DOCS_URL
+console.log({docsURL})
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: "/docs",
+                destination: `${docsURL}/docs`,
+              },
+              {
+                source: "/docs/:path+",
+                destination: `${docsURL}/docs/:path+`,
+              },
+        ]
+    }
+};
 
 export default nextConfig;
